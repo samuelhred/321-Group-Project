e416c7e3a5592f623bbd77a2dbc724afd5f7b46b
Corbin Gossett <cggossett@crimson.ua.edu>
2025-04-23 12:35:19 -0500

    corbin

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a07769ee69ed973fb9637805c32ddd268ea6ccf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d613b639f8511d2f2141e923c115ad2fc135d25")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
