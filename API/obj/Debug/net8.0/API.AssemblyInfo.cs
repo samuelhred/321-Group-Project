05c02b5ae13fcf18953eb8a5291b7caa41c3ef1e
Sam Redelsheimer <samredelsheimer@Sams-MacBook-Air-2.local>
2025-04-24 02:55:50 -0500

    fmmn

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+058f4513263022e69703490abecb3f604e00e864")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d99c919cce3a5055f04c483efa0b6cd83b01d4f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
